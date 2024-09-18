0afc4ec682ce2b58150014d3552e8ae8e364b574
MuminovMuslimbek <muslimbekmuminov47@gmail.com>
2024-09-18 17:43:36 +0300

    carousel dane

--- js/app.js
+++ js/app.js
@@ -81,3 +81,44 @@ function getRandomColor() {
     const colors = ['#ff0', '#f0f', '#0ff', '#f00', '#00f', '#0f0'];
     return colors[Math.floor(Math.random() * colors.length)];
 }
+
+const marqueeContainer = document.querySelector('.marquee-container');
+const marqueeContent = document.querySelector('.marquee-content');
+
+
+marqueeContent.innerHTML += marqueeContent.innerHTML;
+
+let scrollAmount = 0;
+let speed = 1;
+
+function marqueeAnimation() {
+    scrollAmount -= speed;
+    if (Math.abs(scrollAmount) >= marqueeContent.scrollWidth / 1) {
+        scrollAmount = 0;
+    }
+    marqueeContent.style.transform = `translateX(${scrollAmount}px)`;
+    requestAnimationFrame(marqueeAnimation);
+}
+
+marqueeAnimation();
+
+
+const colors = ['#FFBE21', '#4756DF', '#3EC1F3', '#FF7235', '#BB7259', '#34C759', '#FF2D55', '#AF52DE'];
+
+const originalCard = document.querySelector('.carousel-card');
+const container = document.querySelector('.marquee-content');
+
+function createCards(count) {
+    for (let i = 0; i < count; i++) {
+        const newCard = originalCard.cloneNode(true);
+        const color = colors[i % colors.length];
+        newCard.style.borderTop = `10px solid ${color}`;
+
+        container.appendChild(newCard);
+    }
+
+    const clone = container.cloneNode(true);
+    container.appendChild(clone);
+}
+
+createCards(10);
